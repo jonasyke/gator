9638a95eac5bc7f9b816d8cc420bcb721de59807
jonasyke <jonasyke171@gmail.com>
2026-02-13 16:36:42 -0900

    go.mod fix

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main.go
+module github.com/jonasyke/gator
 
 go 1.25.4
--- main.go
+++ main.go
@@ -1,1 +1,28 @@
-package gator
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jonasyke/gator/internal/config"
+)
+
+func main() {
+
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("could not read file: %v", err)
+	}
+
+	err = cfg.SetUser("Jonathan")
+	if err != nil {
+		log.Fatalf("could not set user name: %v", err)
+	}
+
+	final, err := config.Read()
+	if err != nil {
+		log.Fatalf("failed to read final state: %v", err)
+	}
+
+	fmt.Println(final)
+}
